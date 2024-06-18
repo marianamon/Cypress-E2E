9d08ded5192586e47a2f61f337462fbd78800d75
MarianaAlzate <Mariana.Alzate@kinesso.com>
2024-06-18 09:20:02 -0500

    create cypress structure

---
+++ e2e/cypress.config.js
@@ -0,0 +1,38 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig(
+  {
+    chromeWebSecurity: false,
+    video: false,
+    videoUploadOnPasses: false,
+    screenshotOnRunFailure: false,
+    experimentalInteractiveRunEvents: true,
+    experimentalMemoryManagement: true,
+    projectId: 'ryx4xk',
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    pageLoadTimeout: 30000,
+    requestTimeout: 30000,
+    responseTimeout: 30000,
+    numTestsKeptInMemory: 0,
+    
+    e2e: {
+      setupNodeEvents(on, config) {
+        return require('./e2e/cypress/plugins/index.js')(on, config);
+      },
+      env: {
+        allure: true,
+        allureResultsPath: 'cypress/report/allure-result/',
+        allureSkipAutomaticScreenshots: true,
+        allureReuseAfterSpec: true,
+
+      },
+      excludeSpecPattern: '*.js',
+      specPattern: 'cypress/integration/**/*.js',
+    },
+    retries: {
+      runMode: 2,
+      openMode: 2
+    }
+    
+  });
