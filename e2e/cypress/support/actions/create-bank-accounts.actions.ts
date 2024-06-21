e2751c8c3ea6beec06ee1b1b6e7f360eea73c279
Marianamon <marisca86.26@gmail.com>
2024-06-21 13:43:24 -0500

    Create bank accounts (#3)
    * create Accounts Bank Test
    
    * create action by bankAccounts
    
    ---------
    
    Co-authored-by: MarianaAlzate <Mariana.Alzate@kinesso.com>

--- e2e/cypress/support/actions/create-bank-accounts.actions.ts
+++ e2e/cypress/support/actions/create-bank-accounts.actions.ts
@@ -4,15 +4,15 @@ import { CreateBankAccountsPage } from "../../pages/create-bank-accounts.page";
 
 export class CreateBankAccountsActions extends CreateBankAccountsPage {
     createBankAccount(accountName: string, routingNumber: string, accountNumber: string){
-        cy.wait(1000)
+        cy.wait(1000);
         cy.get(this.bankNameInput).type(accountName);
         cy.get(this.routingNumberInput).type(routingNumber);
         cy.get(this.accountNumberInput).type(accountNumber);
     }
 
-    verifyBankAccount(accountName: string){
-        cy.wait(1000)
-        cy.get(this.bankAccountsList).should('not.be.visible').contains(accountName);
+    verifyBankAccount(){
+        cy.wait(3000);
+        cy.get(this.bankAccountsList).should('be.visible');
     }
 
 }
