bd0fcd669d6aa24830f79b36cbe6476e6ae2ef05
MarianaAlzate <Mariana.Alzate@kinesso.com>
2024-06-23 16:15:06 -0500

    change cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -18,7 +18,7 @@ module.exports = defineConfig(
     
     e2e: {
       setupNodeEvents(on, config) {
-        //return require('./e2e/cypress/plugins/index.js')(on, config);
+        return require('./e2e/cypress/plugins/index.js')(on, config);
       },
       env: {
         allure: true,
@@ -30,6 +30,7 @@ module.exports = defineConfig(
       excludeSpecPattern: '*.js',
       specPattern: 'e2e/cypress/e2e/**/*.{feature,features}',
       //supportFile: 'cypress/support/e2e.js',
+      integrationFolder: "e2e/cypress/e2e/**/*.{feature,features}",
       supportFile: false
     },
     retries: {
