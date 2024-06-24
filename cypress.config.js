4cb21d5d3e8410f53830f64f550d6621b146195e
MarianaAlzate <Mariana.Alzate@kinesso.com>
2024-06-24 18:00:48 -0500

    add cypress-cucumber-preprocessor

--- cypress.config.js
+++ cypress.config.js
@@ -1,31 +1,27 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
-module.exports = defineConfig(
-  {
-    chromeWebSecurity: false,
-    video: false,
-    screenshotOnRunFailure: false,
-    experimentalInteractiveRunEvents: true,
-    experimentalMemoryManagement: true,
-    projectId: 'ryx4xk',
-    viewportWidth: 1366,
-    viewportHeight: 768,
-    pageLoadTimeout: 30000,
-    requestTimeout: 30000,
-    responseTimeout: 30000,
-    numTestsKeptInMemory: 0,
-    
-    e2e: {
-      
-      excludeSpecPattern: '*.js',
-      specPattern: 'e2e/cypress/fixtures/*.{feature,features}',
-      //supportFile: 'cypress/support/e2e.js',
-      //integrationFolder: "e2e/cypress/e2e/**/*.{feature,features}",
-      supportFile: false
+module.exports = defineConfig({
+
+  'cypress-cucumber-preprocessor': {
+
+    nonGlobalStepDefinitions: false,
+
+    //step_definitions: './cypress/e2e/login/',
+
+  },
+
+  e2e: {
+
+    setupNodeEvents(on, config) {
+
+      return require('./e2e/cypress/plugins/index.js')(on, config)
+
     },
-    retries: {
-      runMode: 2,
-      openMode: 2
-    },
-    
-  });
+
+    specPattern: 'e2e/cypress/fixtures/*.{feature,features}',
+
+    supportFile:false
+
+  },
+
+})
